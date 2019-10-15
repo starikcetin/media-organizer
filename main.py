1dba2cfb90334fd999d1d8112f4b5189ae7a0daa
starikcetin <cetinsamedtarik@gmail.com>
2019-10-15 19:37:38 +0300

    * option to select whether to move or copy files * handle when destination and source are the same file * options for destination filename formatting

--- main.py
+++ main.py
@@ -24,9 +24,19 @@ def ensure_directories(directory):
         os.makedirs(directory)
 
 
-def copy_file(file, target_directory):
-    return shutil.copy(file, target_directory)
+def handle_file(file, target_directory, shouldMove):
+    str_file = str(Path(file).resolve())
+    str_target_directory = str(Path(target_directory).resolve())
 
+    try:
+        str_result = shutil.copy(str_file, str_target_directory)
+    except shutil.SameFileError:
+        return str_file
+    else:
+        if shouldMove:
+            os.remove(file)
+        return str_result
+        
 
 def make_path(root, datetime):
     directory_schema = datetime.format("YYYY-MM")
@@ -34,14 +44,46 @@ def make_path(root, datetime):
     return Path(result)
 
 
-def rename_file(file, datetime):
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text  # or whatever
+
+
+def rename_file(file, datetime, file_format_mode):
     folder = Path(file).parent
     filename, extension = os.path.splitext(file)
-    new_filename = datetime.format("YYYY-MM-DD HH-mm-ss") + extension
+
+    datetime = datetime.format("YYYY-MM-DD HH-mm-ss")
+
+    # 1: date only
+    # 2: preserve filenames
+    # 3: date + old filename, do not duplicate date and remove excess whitespace
+    # 4: date + old filename, do not alter old filename
+    if file_format_mode is 1:
+        new_filename = datetime + extension
+    
+    elif file_format_mode is 2:
+        new_filename = filename + extension
+    
+    elif file_format_mode is 3:
+        filename = remove_prefix(filename.strip(), datetime).strip()
+        new_filename = datetime + " " + filename + extension
+    
+    elif file_format_mode is 4:
+        new_filename = datetime + " " + filename + extension
+    
+    else:
+        raise Exception("Filename format is invalid")
+
     new_file = os.path.join(folder, new_filename)
     new_file = uniquify(new_file)
     os.rename(file, new_file)
     return new_file
+
+
+def are_same_file(path1, path2):
+    return Path(path1).resolve().samefile(Path(path2).resolve())
 
 
 def get_date_taken(file_path):
@@ -72,37 +114,55 @@ def get_date_taken(file_path):
             return pendulum.from_timestamp(dt.timestamp())
 
 
-def process_file(file_path, target_root):
+def process_file(file_path, target_root, shouldMove, file_format_mode):
     try:
         date_taken = get_date_taken(file_path)
+        
+        if date_taken is None:
+            raise Exception("date_taken is None")
+
         print(date_taken)
         target_path = make_path(target_root, date_taken)
         ensure_directories(target_path)
-        new_file = copy_file(file_path, target_path)
-        new_file_renamed = rename_file(new_file, date_taken)
-        print(new_file_renamed)
+        new_file = handle_file(file_path, target_path, shouldMove)
+        new_file_renamed = rename_file(new_file, date_taken, file_format_mode)
+        
+        if are_same_file(file_path, new_file_renamed):
+            print("Destination and source are the same file.")
+        else:
+            print(new_file_renamed)
+
     except Exception as ex:
         print("Error while parsing the date taken: " + str(ex))
         target_path = os.path.join(target_root, "Could not categorize")
         ensure_directories(target_path)
-        new_file = copy_file(file_path, target_path)
-        print(new_file)
+        new_file = handle_file(file_path, target_path, shouldMove)
+
+        if are_same_file(file_path, new_file):
+            print("Destination and source are the same file.")
+        else:
+            print(new_file)
 
 
-def process_root(source_root, target_root):
+def process_root(source_root, target_root, shouldMove, file_format_mode):
     for filename in Path(source_root).glob('**/*'):
         if filename.is_dir():
             print("directory")
             continue
         print(filename)
-        process_file(filename, target_root)
+        process_file(filename, target_root, shouldMove, file_format_mode)
         print()
 
 
 def main():
     source_root = input("Source root: ")
     target_root = input("Target root: ")
-    process_root(source_root, target_root)
+    move_or_copy = input("Move or copy? (m/c): ")
+    file_format_mode = int(input("Filename format of the new files? (1: date only / 2: preserve filenames / 3: date + old filename, do not duplicate date and remove excess whitespace / 4: date + old filename, do not alter old filename) (1/2/3/4): "))
+
+    shouldMove = (move_or_copy is 'm') & (move_or_copy is not 'c')
+
+    process_root(source_root, target_root, shouldMove, file_format_mode)
 
 
 if __name__ == "__main__":
